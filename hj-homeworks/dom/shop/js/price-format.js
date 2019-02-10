16479b413a2084a28075254c5da3f457eb64396d
Olga <olga.myazina@outlook.com>
2019-02-10 22:57:11 +0300

    task 2

--- hj-homeworks/dom/shop/js/price-format.js
+++ hj-homeworks/dom/shop/js/price-format.js
@@ -1,3 +1,25 @@
 function getPriceFormatted(value) {
-  return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
+
+'use strict'
+
+let nodeCount, nodePrice, price = 0;
+
+function init() {
+  const container = document.getElementById('container');
+  container.addEventListener('click', addProduct);
+  nodeCount = document.getElementById('cart-count');
+  nodePrice = document.getElementById('cart-total-price')
+}
+
+function addProduct(e) {
+  if (e.target.tagName === 'BUTTON') {
+    nodeCount.textContent = +nodeCount.innerText + 1;
+    price += parseInt(e.target.getAttribute('data-price'));
+    nodePrice.textContent = getPriceFormatted(price);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init);
+

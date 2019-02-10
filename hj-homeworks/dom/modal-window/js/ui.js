1e76a6753982029414540b0be45f730224b695f3
Olga <olga.myazina@outlook.com>
2019-02-10 23:11:01 +0300

    task 3

--- hj-homeworks/dom/modal-window/js/ui.js
+++ hj-homeworks/dom/modal-window/js/ui.js
@@ -1,2 +1,9 @@
 // Инициализация функционала модального окна
 initModalUi();
+
+function init() {
+  const button = document.getElementsByClassName('btn')[0];
+  button.addEventListener('click', modal);
+}
+
+document.addEventListener('DOMContentLoaded', initModalUi);

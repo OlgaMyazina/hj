1951813c881a1ef895cc9d89733b9cf0d1598845
Olga <olga.myazina@outlook.com>
2019-02-13 21:10:24 +0300

    task1: weather-widget

--- hj-homeworks/xhr/weather-widget/js/main.js
+++ hj-homeworks/xhr/weather-widget/js/main.js
@@ -1,7 +1,22 @@
+'use strict'
+
 const request = new XMLHttpRequest();
-request.open('GET', 'https://neto-api.herokuapp.com/weather', false);
+request.open('GET', 'https://neto-api.herokuapp.com/weather', true);
+request.addEventListener('load', onLoad);
+request.addEventListener('error', onError);
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+
+function onLoad() {
+  if (request.status === 200) {
+    try {
+      const response = JSON.parse(request.responseText);
+      setData(response);
+    } catch (err) {
+      console.log(`Ошибка JSON: ${err}`);
+    }
+  }
+}
+
+function onError() {
+  console.log('Ошибка при выполнении запроса');
 }

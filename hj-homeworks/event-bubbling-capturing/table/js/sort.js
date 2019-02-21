30b2826f6965a1ac65612fec72a06496762aca7e
Olga <olga.myazina@outlook.com>
2019-02-21 23:00:20 +0300

    task 3

--- hj-homeworks/event-bubbling-capturing/table/js/sort.js
+++ hj-homeworks/event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.getAttribute('data-prop-name')) sortByTh(event);
+}
+
+function sortByTh(event) {
+  const thName = event.target.getAttribute('data-prop-name');
+  let dir = event.target.getAttribute('data-dir');
+  dir = (dir == '1') ? '-1' : '1';
+  table.setAttribute('data-sort-by', thName);
+  event.target.setAttribute('data-dir', dir);
+  sortTable(thName, dir);
 }

30277626e9a56c73d0e7ed7982caf623e3c04dac
Olga <olga.myazina@outlook.com>
2019-02-27 17:29:14 +0300

    task 1

--- hj-homeworks/websocket/bubbles/js/click.js
+++ hj-homeworks/websocket/bubbles/js/click.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+connection.addEventListener('open', () => {
+  showBubbles(connection);
+  console.log('Вебсокет-соединение открыто');
+});
+
+connection.addEventListener('close', event => {
+  console.log('Вебсокет-соединение закрыто');
+});
+
+connection.addEventListener('error', error => {
+  console.log(`Произошла ошибка: ${error.data}`);
+});
+
+document.addEventListener("click", event => {
+  const coordinates = { x: event.clientX, y: event.clientY };
+  connection.send(JSON.stringify(coordinates));
+});
+
